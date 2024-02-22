408dff102bbf7393aa6bffceaf8fabd91d4a6aaf
LuisPalacios <>
2024-02-22 15:44:35 +0000

    LuisPalacios published a site update

---
+++ search/all_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['set_5',['set',['../my__lib_2CMakeLists_8txt.html#abc4a43310046a92baeb76452c0d7ee5c',1,'set(LIBRARY_SOURCES &quot;${CMAKE_CURRENT_SOURCE_DIR}/my_lib.cc&quot;) set(LIBRARY_HEADERS &quot;$:&#160;CMakeLists.txt'],['../my__lib_2CMakeLists_8txt.html#a3ddbfbe904bdf0d62178df205c3e98ac',1,'set(LIBRARY_INCLUDES &quot;./&quot; &quot;${CMAKE_BINARY_DIR}/configured_files/include&quot;) add_library($:&#160;CMakeLists.txt']]]
+];
